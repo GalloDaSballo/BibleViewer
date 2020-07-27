698518b75872494ff9c22f0177cd69d83304d40c
Entreprenerd <alex@entreprenerd.xyz>
2020-07-27 12:58:00 +0200

    Now Db works properly

--- data/db-config.js
+++ data/db-config.js
@@ -1,50 +1,17 @@
-const sqlite = require("sqlite");
-const sqlite3 = require("sqlite3");
-
-// trying to get 4.0.12 version of sqlite package working
-// significant changes from v3 with what feels like a lot more moving parts
-// https://www.npmjs.com/package/sqlite#opening-the-database
-
-// ATTEMPT 1
-// ATTEMPT 1 RESULT: error no such table + error async function rejecting promise without catch block
-// const setup = async () => {
-//   const db = await sqlite.open({
-//     filename: "./bible-sqlite.db",
-//     driver: sqlite3.Database,
-//   });
-
-//   const verse = await db.all("SELECT id, t FROM t_asv WHERE id");
-//   console.log(JSON.stringify(verse, null, 2));
-// };
-
-// setup();
-
-// ATTEMPT 2
-// ATTEMPT 2 RESULT: error no such table + error async function rejecting promise without catch block
-// const setup = async () => {
-//   db = await sqlite
-//     .open({
-//       filename: "./bible-sqlite.db",
-//       driver: sqlite3.Database,
-//     })
-//     .then(async (db) => {
-//       const verse = await db.all("SELECT id, t FROM t_asv WHERE id");
-//       console.log(JSON.stringify(verse, null, 2));
-//     });
-// };
-
-// setup();
-
-// ATTEMPT 3
-// ATTEMPT 3 RESULT: SyntaxError: Missing initializer in const declaration
-// const async openDb = () => {
-//   return await sqlite.open({
-//     filename: "./bible-sqlite.db",
-//     driver: sqlite3.Database,
-//   });
-// };
-
-// openDb().then((db) => {
-//   const verse = db.all("SELECT id, t FROM t_asv WHERE id");
-//   console.log(JSON.stringify(verse, null, 2));
-// });
+const knex = require("knex")({
+    client: "sqlite3",
+    connection: {
+      filename: "./data/bible-sqlite.db",
+    },
+    useNullAsDefault: true,
+  });
+  
+  // for debug purposes, show a verse when connecting
+  knex
+    .from("t_asv")
+    .select("id", "c", "v", "t")
+    .where("id", "1001003")
+    .then((data) => console.log("data retreived:", data))
+    .catch((err) => console.log(err));
+  
+  module.exports = knex;
--- pages/api/verse/[verseId].js
+++ pages/api/verse/[verseId].js
@@ -1,7 +1,14 @@
-export default getVerse = (req, res) => {
+import db from '../../../data/db-config'
+export default async (req, res) => {
   if (req.method !== "GET") {
     res.status(500).json({ message: "sorry we only accept GET requests" });
   }
+  const verse = await db
+    .from("t_asv")
+    .select("id", "c", "v", "t")
+    .where("id", req.query.verseId)
+
+  console.log("result is", verse)
 
   res.statusCode = 200;
   res.json({ byId: req.query.verseId });
